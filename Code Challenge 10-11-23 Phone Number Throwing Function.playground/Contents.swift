205668c32d203ca27f3fdffb7c96dfaca8772cbc
LionOfPanjshir <a.higbee@gmail.com>
2023-10-11 19:19:34 -0600

    10-11-23 Code challenge updated

--- Code Challenge 10-11-23 Phone Number Throwing Function.playground/Contents.swift
+++ Code Challenge 10-11-23 Phone Number Throwing Function.playground/Contents.swift
@@ -2,19 +2,47 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-//func isPhoneNumber(number: String) -> Bool {
-//    let phoneNumberRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[0-9]).{9,}$")
-//    
-//    return phoneNumberRegex.evaluate(with: number)
-//}
-//
-//var phoneNumber = "8014536402"
-//
-//if isPhoneNumber(number: phoneNumber) {
-//    var formattedNumber = ""
-//    var numberArray = number.
-//}
+enum numberError: Error {
+    case numberError
+}
 
+func isPhoneNumber(number: String) -> Bool {
+    let phoneNumberRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[0-9]).{10,}$")
+    
+    return phoneNumberRegex.evaluate(with: number)
+}
+
+func formatNumber(number: String) throws -> String {
+    if isPhoneNumber(number: number) {
+        var formattedNumber = ""
+        //var numberArray = number.components(separatedBy: "")
+        var numberArray = [String]()
+        for char in number {
+            numberArray.append(String(char))
+        }
+        for index in 0..<numberArray.count {
+            if index == 3 || index == 6 {
+                formattedNumber.append("-")
+                formattedNumber.append(numberArray[index])
+            } else {
+                formattedNumber.append(numberArray[index])
+            }
+        }
+        return formattedNumber
+    } else {
+        throw numberError.numberError //"Please enter a valid phone number."
+    }
+}
+
+var phoneNumber = "8014536409"
+do {
+    var number = try formatNumber(number: phoneNumber)
+    print(number)
+} catch numberError.numberError {
+    print("Please enter a valid number.")
+}
+
+//Other solutions
 //enum NumberError: Error {
 //    case tooShort
 //    case tooLong
