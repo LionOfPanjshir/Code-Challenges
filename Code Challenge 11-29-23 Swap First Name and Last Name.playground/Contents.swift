a2301f71c706a8783cdbe1e9f3550f8b4476427e
LionOfPanjshir <a.higbee@gmail.com>
2023-11-29 07:40:11 -0700

    Swapped Names Code Challenge 11-29 added

---
+++ Code Challenge 11-29-23 Swap First Name and Last Name.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var nameString = "Andrew Higbee"
+
+func swapNames(name: String) -> String {
+    var nameArray = name.components(separatedBy: " ")
+    nameArray.swapAt(0, 1)
+    var swappedName = nameArray[0] + " " + nameArray[1]
+    return swappedName
+}
+
+print(swapNames(name: nameString))
