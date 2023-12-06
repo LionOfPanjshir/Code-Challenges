99e3fb628fc3523f4a53cb6c9379da7c39d43d26
LionOfPanjshir <a.higbee@gmail.com>
2023-12-06 07:56:31 -0700

    Code Challenge 12-6-23 added
    Human, Dog, and Cat years

---
+++ Code Challenge 12-6-23 Cat and Dog Years Conversion.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// I have a cat and a dog.
+
+// I got them at the same time as a kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+
+func animalYears(humanYears: Int) -> [Int] {
+    
+    if humanYears == 1 {
+        return [humanYears, 15, 15]
+    } else if humanYears == 2 {
+        return [humanYears, 24, 24]
+    } else if humanYears >= 3 {
+        return [humanYears, 15 + 9 + (humanYears - 2) * 4, 15 + 9 + (humanYears - 2) * 5]
+    }
+    
+    else { return [0] }
+}
+
+animalYears(humanYears: 5)
+
+animalYears(humanYears: 10)
